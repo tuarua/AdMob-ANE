35dc69345e7a35984516ad7d419668efc0b35ce5
Eoin Landy <el111@yahoo.com>
2017-09-10 11:33:10 +0100

    no message

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -4,4 +4,8 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/1.1.0/ios_depende
 unzip -u ios_dependencies.zip
 rm ios_dependencies.zip
 
+wget https://github.com/tuarua/AdMob-ANE/releases/download/0.0.3/ios_dependencies.zip
+unzip -u ios_dependencies.zip
+rm ios_dependencies.zip
+
 wget -O ../native_extension/ane/AdMobANE.ane https://github.com/tuarua/AdMob-ANE/releases/download/0.0.3/AdMobANE.ane?raw=true
