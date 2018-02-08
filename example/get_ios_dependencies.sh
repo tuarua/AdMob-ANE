2a23370fb719bcdcae39e97e34de579da6a11385
Eoin Landy <el111@yahoo.com>
2018-02-08 22:20:04 +0000

    1.3.0 read to merge

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -4,8 +4,8 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.2.0/ios_depende
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/AdMob-ANE/releases/download/1.2.0/ios_dependencies.zip
+wget https://github.com/tuarua/AdMob-ANE/releases/download/1.3.0/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/AdMobANE.ane https://github.com/tuarua/AdMob-ANE/releases/download/1.2.0/AdMobANE.ane?raw=true
+wget -O ../native_extension/ane/AdMobANE.ane https://github.com/tuarua/AdMob-ANE/releases/download/1.3.0/AdMobANE.ane?raw=true
